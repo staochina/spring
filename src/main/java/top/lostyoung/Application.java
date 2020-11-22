bb885322120ff5825abfa3ef1f647da895a64e60
sun.tao <sun.tao@lifecycle.cn>
2020-11-22 09:44:13 +0800

    init

---
+++ src/main/java/top/lostyoung/Application.java
@@ -0,0 +1,10 @@
+package top.lostyoung;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
